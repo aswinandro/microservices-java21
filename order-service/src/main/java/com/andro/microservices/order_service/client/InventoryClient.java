b37aa8bc282f9569f8686c8281fd8f7f56ca6e50
Aswin Andro <aswin.knight@gmail.com>
2025-10-04 15:01:29 +0400

    fix Resilience4j
    fix Resilience4j

--- order-service/src/main/java/com/andro/microservices/order_service/client/InventoryClient.java
+++ order-service/src/main/java/com/andro/microservices/order_service/client/InventoryClient.java
@@ -1,12 +1,26 @@
 package com.andro.microservices.order_service.client;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "inventory", url="${inventory.url}")
+@FeignClient(value = "inventory", url = "${inventory.url}")
 public interface InventoryClient {
+
+    Logger log = LoggerFactory.getLogger(InventoryClient.class);
+
     @RequestMapping(method = RequestMethod.GET, value = "/api/inventory")
-    boolean isInStock(@RequestParam("skucode") String skuCode, @RequestParam Integer quantity);
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+    @Retry(name = "inventory")
+    boolean isInStock(@RequestParam("skucode") String skuCode, @RequestParam("quantity") Integer quantity);
+
+    default boolean fallbackMethod(String skuCode, Integer quantity, Throwable throwable) {
+        log.info("Cannot get inventory for skucode {} failure reason: {}", skuCode, throwable.getMessage());
+        return false;
+    }
 }
