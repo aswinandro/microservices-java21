fd609d6819cede62304563e1528f990cbdf9fe43
Aswin Andro <aswin.knight@gmail.com>
2025-10-05 14:53:13 +0400

    kafka
    kafka event

--- order-service/src/main/java/com/andro/microservices/order_service/service/OrderService.java
+++ order-service/src/main/java/com/andro/microservices/order_service/service/OrderService.java
@@ -2,9 +2,11 @@ package com.andro.microservices.order_service.service;
 
 import com.andro.microservices.order_service.client.InventoryClient;
 import com.andro.microservices.order_service.dto.OrderRequest;
+import com.andro.microservices.order_service.event.OrderPlacedEvent;
 import com.andro.microservices.order_service.model.Order;
 import com.andro.microservices.order_service.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -14,19 +16,24 @@ import java.util.UUID;
 public class OrderService {
     private final OrderRepository orderRepository;
     private final InventoryClient inventoryClient;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
     public void placeOrder(OrderRequest orderRequest) {
-        var isProductInStock = inventoryClient.isInStock(orderRequest.skucode(), orderRequest.quantity());
+        var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
 //        map OrderRequest to Order Object
         if (isProductInStock) {
             Order order = new Order();
             order.setOrderNumber(UUID.randomUUID().toString());
             order.setPrice(orderRequest.price());
-            order.setSkuCode(orderRequest.skucode());
+            order.setSkuCode(orderRequest.skuCode());
             order.setQuantity(orderRequest.quantity());
             orderRepository.save(order);
+
+//            Send Message to Kafka Topic - order Number and mail
+            OrderPlacedEvent orderPlacedEvent = new OrderPlacedEvent(order.getOrderNumber(),orderRequest.userDetails().email());
+            kafkaTemplate.send("order-paced", orderPlacedEvent);
         }
         else{
-            throw new RuntimeException("Product" + orderRequest.skucode() + "is not in stock, please try again later");
+            throw new RuntimeException("Product" + orderRequest.skuCode() + "is not in stock, please try again later");
         }
     }
 
